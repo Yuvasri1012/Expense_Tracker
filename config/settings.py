bd5510b0104533a3419d9aee9857f3c2639a106d
Yuvasri1012 <yuvasrir120@gmail.com>
2026-05-17 13:18:08 +0530

    Render Database Fix

--- config/settings.py
+++ config/settings.py
@@ -5,6 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-expense-tracker-secret-key-change-in-production'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+import dj_database_url
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -50,19 +54,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Make sure MySQL is running and you have created the database:
 #   CREATE DATABASE expense_tracker CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 # Update NAME, USER, PASSWORD below to match your MySQL setup.
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'expense_tracker',      
+#         'USER': 'root',                  
+#         'PASSWORD': 'Admin@123',     
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'expense_tracker',       # ← your database name
-        'USER': 'root',                  # ← your MySQL username
-        'PASSWORD': 'Admin@123',     # ← your MySQL password
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL"), 
+            conn_max_age=600, 
+            ssl_require=True 
+        )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
